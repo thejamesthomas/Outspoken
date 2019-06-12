2f9584c8fa260f1c6058d8a4dcf156a7089d4e0f
James Thomas <thejamesthomas@gmail.com>
2019-06-11 20:57:51 -0500

    Initial Release of Outspoken

---
+++ Outspoken/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Media.SpeechSynthesis;
+using Outspoken.ViewModel;
+using System.Collections.ObjectModel;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Outspoken
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MediaElement _media;
+        SpeechSynthesizer _speech;
+
+        ObservableCollection<VoiceViewModel> Voices = VoiceViewModel.PopulateVoices();
+        VoiceViewModel SelectedVoice;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            _media = new MediaElement();
+            _speech = new SpeechSynthesizer();
+
+            SelectedVoice = Voices[0];
+            _speech.Voice = SelectedVoice.VoiceInformation;
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            SpeakAndClearAsync();
+        }
+
+        private void MessageBox_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                SpeakAndClearAsync();
+            }
+        }
+
+        private async void SpeakAndClearAsync()
+        {
+            SpeechSynthesisStream stream = await _speech.SynthesizeTextToStreamAsync(MessageBox.Text);
+
+            _media.SetSource(stream, stream.ContentType);
+            _media.Play();
+
+            MessageBox.Text = "";
+        }
+
+        private void SelectedVoiceComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedVoice = e.AddedItems[0] as VoiceViewModel;
+            _speech.Voice = SelectedVoice.VoiceInformation;
+        }
+
+        private void AudioPitch_Slider_ValueChanged(object sender, Windows.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e)
+        {
+            if (SelectedVoice != null)
+            {
+                SelectedVoice.AudioPitch = e.NewValue;
+                _speech.Options.AudioPitch = SelectedVoice.AudioPitch;
+            }
+        }
+
+        private void SpeakingRate_Slider_ValueChanged(object sender, Windows.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e)
+        {
+            if (SelectedVoice != null)
+            {
+                SelectedVoice.SpeakingRate = e.NewValue;
+                _speech.Options.SpeakingRate = SelectedVoice.SpeakingRate;
+            }
+        }
+    }
+}
---
+++ Outspoken/ViewModel/VoiceViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.SpeechSynthesis;
+
+namespace Outspoken.ViewModel
+{
+    class VoiceViewModel
+    {
+        private VoiceInformation _voiceInformation;
+
+        public VoiceViewModel(VoiceInformation voiceInformation)
+        {
+            this._voiceInformation = voiceInformation;
+        }
+
+        public double AudioPitch { get; internal set; }
+        public double SpeakingRate { get; internal set; }
+
+        public string DisplayName {
+            get { return this._voiceInformation.DisplayName; }
+        }
+
+        public VoiceInformation VoiceInformation
+        {
+            get { return _voiceInformation; }
+        }
+
+        internal static ObservableCollection<VoiceViewModel> PopulateVoices()
+        {
+            var voices = new ObservableCollection<VoiceViewModel>();
+            foreach (var voice in SpeechSynthesizer.AllVoices)
+            {
+                voices.Add(new VoiceViewModel(voice));
+            }
+            return voices;
+        }
+    }
+}
